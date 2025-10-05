64623b1271376cb0d60eba27bbfae039936d474a
Narayan Rajkumar Suryawanshi <178514243+NARAYAN49-tech@users.noreply.github.com>
2025-10-05 21:09:37 +0530

    Update ButterflyPattern.java

--- ButterflyPattern.java
+++ ButterflyPattern.java
@@ -1,16 +1,21 @@
 public class ButterflyPattern {
     public static void main(String[] args) {
         int rows = 5;
+        /* Printing first 5 rows */
         for (int i = 1; i <= rows; i++) {
+            /* printing first part */
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
+            /* Printing spaces */
             for (int j = 1; j <= 2 * (rows - i); j++) {
                 System.out.print(" ");
             }
+            /* Printing second part  */
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
+            /* Stepping to next line */
             System.out.println();
         }
         for (int i = rows; i >= 1; i--) {
@@ -27,3 +32,4 @@ public class ButterflyPattern {
         }
     }
 }
+
